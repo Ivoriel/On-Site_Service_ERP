ad79c390c3f2dd801484505ac177b1c261070b66
Ivoriel <tkivor@gmail.com>
2021-09-13 23:03:48 +0200

    Basic Service Request Service functionality

--- src/main/java/pl/kosinski/serviceRequest/ServiceRequestCrudAdapter.java
+++ src/main/java/pl/kosinski/serviceRequest/ServiceRequestCrudAdapter.java
@@ -3,6 +3,7 @@ package pl.kosinski.serviceRequest;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -30,26 +31,53 @@ public class ServiceRequestCrudAdapter implements ServiceRequestCrudService {
 
     @Override
     public ServiceRequestInfoDto findServiceRequestbyId(long id) {
-        return null;
+        return translateToDto(getById(id));
     }
 
     @Override
     public void deleteServiceRequest(long id) {
-
+        repository.delete(getById(id));
     }
 
     @Override
     public List<ServiceRequestInfoDto> findAllServiceRequests() {
-        return null;
+        List<ServiceRequest> requestList = repository.findAll();
+        List<ServiceRequestInfoDto> requestInfoDtoList = new ArrayList<>();
+        for (ServiceRequest request : requestList) {
+            requestInfoDtoList.add(translateToDto(request));
+        }
+        return requestInfoDtoList;
     }
 
     @Override
     public List<ServiceRequestInfoDto> getServiceReqByClientId(long id) {
-        return null;
+        List<ServiceRequest> requestList = repository.getServiceReqByClientId(id);
+        List<ServiceRequestInfoDto> requestInfoDtoList = new ArrayList<>();
+        for (ServiceRequest request : requestList) {
+            requestInfoDtoList.add(translateToDto(request));
+        }
+        return requestInfoDtoList;
     }
 
     @Override
     public List<ServiceRequestInfoDto> getServiceReqByUnitId(long id) {
-        return null;
+        List<ServiceRequest> requestList = repository.getServiceReqByUnitId(id);
+        List<ServiceRequestInfoDto> requestInfoDtoList = new ArrayList<>();
+        for (ServiceRequest request : requestList) {
+            requestInfoDtoList.add(translateToDto(request));
+        }
+        return requestInfoDtoList;
+    }
+
+    private ServiceRequestInfoDto translateToDto(ServiceRequest request) {
+        ServiceRequestInfoDto requestDto = new ServiceRequestInfoDto();
+        requestDto.setId(request.getId());
+        requestDto.setType(request.getType());
+        requestDto.setStatus(request.getStatus());
+        requestDto.setBrief(request.getBrief());
+        requestDto.setDebrief(request.getDebrief());
+        requestDto.setUnits(request.getUnits());
+        requestDto.setTasks(request.getServiceTasks());
+        return requestDto;
     }
 }
--- src/main/java/pl/kosinski/serviceRequest/ServiceRequestRepository.java
+++ src/main/java/pl/kosinski/serviceRequest/ServiceRequestRepository.java
@@ -3,6 +3,12 @@ package pl.kosinski.serviceRequest;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ServiceRequestRepository extends JpaRepository<ServiceRequest, Long> {
+
+    List<ServiceRequest> getServiceReqByClientId(long id);
+
+    List<ServiceRequest> getServiceReqByUnitId(long id);
 }
