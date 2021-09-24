db5d391c702bf2df7dec8870acbe4123732b1e34
Ivoriel <tkivor@gmail.com>
2021-09-24 16:15:15 +0200

    URL address change for units jsp forms.

--- src/main/java/pl/kosinski/unit/UnitCrudController.java
+++ src/main/java/pl/kosinski/unit/UnitCrudController.java
@@ -12,7 +12,7 @@ import javax.validation.Valid;
 import java.util.List;
 
 @Controller
-@RequestMapping("/units")
+@RequestMapping("/units/form")
 @AllArgsConstructor
 public class UnitCrudController {
 
