dcc4d3940207a4572d658bebec0d73a647b00314
Ivoriel <tkivor@gmail.com>
2021-10-13 15:18:11 +0200

    Unnecessary imports deleted.

--- src/main/java/pl/kosinski/client/ClientConverter.java
+++ src/main/java/pl/kosinski/client/ClientConverter.java
@@ -3,8 +3,6 @@ package pl.kosinski.client;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.convert.converter.Converter;
 import org.springframework.stereotype.Component;
-import pl.kosinski.task.TaskCrudService;
-import pl.kosinski.task.TaskInfoDto;
 
 @Component
 public class ClientConverter implements Converter<String, ClientInfoDto> {
